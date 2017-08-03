bf3a1148d82b8f2bffd2a4acae03a23e8dedb761
Dan Morrison <danielroy.morrison@gmail.com>
2017-08-04 10:55:00 +1200

    add further styling to cv components and add js for expanding collapsing sections on button press

--- js/site.js
+++ js/site.js
@@ -1,1 +1,15 @@
+window.onload = function(){
+  var cvSectionButtons = document.getElementsByClassName("cv-button");
+  var i;
 
+  for (i = 0; i < cvSectionButtons.length; i++) {
+    cvSectionButtons[i].onclick = function() {
+      var contentContainer = this.nextElementSibling;
+      if (contentContainer.style.maxHeight){
+        contentContainer.style.maxHeight = null;
+      } else {
+        contentContainer.style.maxHeight = contentContainer.scrollHeight + "px";
+      }
+    }
+  }
+}
